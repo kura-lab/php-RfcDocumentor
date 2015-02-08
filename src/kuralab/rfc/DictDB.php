36d993a824a4506508d2468fa1d5b7a6e4492d62
kura-lab <kmasaru86@gmail.com>
2015-02-08 21:37:14 +0900

    added selectWord method and modified search method.

--- src/kuralab/rfc/DictDB.php
+++ src/kuralab/rfc/DictDB.php
@@ -17,23 +17,42 @@ class DictDB
         $this->dbh = null;
     }
 
-    public function search($word = "")
+    public function search($word)
     {
         $sql = 'select * from items where word=?';
+        $result = $this->selectWord($sql, $word);
+
+        if (!empty($result)) return $result;
+
+        $length = strlen($word);
+        for ($i = 1; $i < $length - 3; $i++) {
+            $word = substr($word, 0, $length - $i);
+            $sql = "select * from items where word LIKE ? || '" . str_repeat("%", $i) . "'";
+            $result = $this->selectWord($sql, $word);
+            if (!empty($result)) return $result;
+        }
+
+        return array();
+    }
+
+    public function selectWord($sql, $word)
+    {
         try {
             if ($this->dbh == null) {
-                return "";
+                return array();
             }
             $this->dbh->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
             $sth = $this->dbh->prepare($sql);
             $sth->setFetchMode(\PDO::FETCH_NUM);
             $sth->bindParam(1, $word, \PDO::PARAM_STR);
             $sth->execute();
-            while ($result = $sth->fetch()) {
-                return $result;
+            $result = array();
+            while ($retval = $sth->fetch()) {
+                $result[$retval[1]] = $retval[2];
             }
+            return $result;
         } catch (\PDOException $e) {
-            return "";
+            return array();
         }
     }
 }
